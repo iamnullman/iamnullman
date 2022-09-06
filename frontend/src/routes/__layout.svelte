717c9df60f55fad106376fa70a5a0a3b37bbc7be
NullMan <73245847+iamnullman@users.noreply.github.com>
2022-09-06 04:19:32 +0300

    delete

--- frontend/src/routes/__layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import "../app.css";
-  import 'tippy.js/dist/tippy.css';
-  import Footer from "../components/Footer/footer.svelte";
-</script>
- <main class="overflow-y-hidden md:overflow-y-visible min-h-[calc(100vh-14px)] max-w-screen-lg p-5 w-full md:py-10 md:w-10/12 lg:py-1 lg:w-[800px] mx-auto transition-all duration-300">
-<slot />
-<Footer/>
-</main>
--- frontend/src/routes/about.svelte
+++
@@ -1,8 +0,0 @@
-<script context="module" lang="ts">
-	export const prerender = true;
-	import About from "../components/About/index.svelte";
-</script>
-
-<div class="items-center justify-center p-10">
-	<About />
-</div>
